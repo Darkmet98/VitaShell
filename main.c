836a27e0b33a17a37227dd43181a321fffcb8e07
Darkmet98 <iinfernape1998@outlook.es>
2016-09-12 21:17:13 +0200

    Revert "Deleted "Brick Mode""
    This reverts commit 6a68984ed8633d39416b942c5cc35d1017bec28d.

--- main.c
+++ main.c
@@ -1265,7 +1265,7 @@ void fileBrowserMenuCtrl() {
 	void *buf = malloc(0x100);
 	// Mount RW Partitions - Originaly writted for tomtomdu80
 	if (current_buttons & SCE_CTRL_LTRIGGER && current_buttons & SCE_CTRL_RTRIGGER && current_buttons & SCE_CTRL_SQUARE) { // L + R + Square
-		// Init Mount vs0 partition in RW.
+		// Init Mount Unsafe Mode RW Partitions	
 		{
 			int i;
 			sceKernelDelayThread(0 * 1000 * 1000);
@@ -1275,6 +1275,31 @@ void fileBrowserMenuCtrl() {
 			_vshIoMount(0x300, 0, 2, buf); // Mount only vs0 partition in RW
 		}
 	}
+	if (current_buttons & SCE_CTRL_LTRIGGER && current_buttons & SCE_CTRL_RTRIGGER && current_buttons & SCE_CTRL_TRIANGLE) { // L + R + Triangle
+		// Init Mount Brick Mode RW Partitions
+		{
+			int i;
+			sceKernelDelayThread(0 * 1000 * 1000); // Dismount all system partitions
+			vshIoUmount(0x00, 0, 0, 0);
+			vshIoUmount(0x100, 0, 0, 0);
+			vshIoUmount(0x200, 0, 0, 0);
+			vshIoUmount(0x300, 0, 0, 0);
+			vshIoUmount(0x400, 0, 0, 0);
+			vshIoUmount(0x500, 0, 0, 0);
+			vshIoUmount(0x600, 0, 0, 0);
+			vshIoUmount(0x700, 0, 0, 0); 
+			
+			sceKernelDelayThread(0 * 1000 * 1000); // Mount all system partitions in RW
+			_vshIoMount(0x00, 0, 2, buf);
+			_vshIoMount(0x100, 0, 2, buf);
+			_vshIoMount(0x200, 0, 2, buf);
+			_vshIoMount(0x300, 0, 2, buf);
+			_vshIoMount(0x400, 0, 2, buf);
+			_vshIoMount(0x500, 0, 2, buf);
+			_vshIoMount(0x600, 0, 2, buf);
+			_vshIoMount(0x700, 0, 2, buf);
+		}
+	}		
 }
 
 int shellMain() {
